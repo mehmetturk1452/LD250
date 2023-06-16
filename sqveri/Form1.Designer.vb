c8e0b9d57aa7fe81081147a37fbd279d7f5d37d6
Mehmet TÜRK <Mehmet TRK@MEHMET-TURK-PC>
2023-06-16 23:42:20 +0300

    Proje dosyası ekle.

---
+++ sqveri/Form1.Designer.vb
@@ -0,0 +1,216 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form, bileşen listesini temizlemeyi bırakmayı geçersiz kılar.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form Tasarımcısı tarafından gerektirilir
+    Private components As System.ComponentModel.IContainer
+
+    'NOT: Aşağıdaki yordam Windows Form Tasarımcısı için gereklidir
+    'Windows Form Tasarımcısı kullanılarak değiştirilebilir.  
+    'Kod düzenleyicisini kullanarak değiştirmeyin.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.Btnbaglan = New System.Windows.Forms.Button()
+        Me.CheckBox200 = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PLC_okumaTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'timer1
+        '
+        Me.timer1.Interval = 5000
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(9, 107)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(323, 307)
+        Me.PictureBox1.TabIndex = 138
+        Me.PictureBox1.TabStop = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(350, 107)
+        Me.DataGridView1.Margin = New System.Windows.Forms.Padding(2)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(938, 304)
+        Me.DataGridView1.TabIndex = 139
+        '
+        'btnStop
+        '
+        Me.btnStop.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.btnStop.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.btnStop.ForeColor = System.Drawing.Color.Black
+        Me.btnStop.Location = New System.Drawing.Point(694, 62)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(79, 41)
+        Me.btnStop.TabIndex = 140
+        Me.btnStop.Text = "Kapat"
+        Me.btnStop.UseVisualStyleBackColor = False
+        '
+        'Btnbaglan
+        '
+        Me.Btnbaglan.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Btnbaglan.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Btnbaglan.ForeColor = System.Drawing.Color.Black
+        Me.Btnbaglan.Location = New System.Drawing.Point(350, 62)
+        Me.Btnbaglan.Name = "Btnbaglan"
+        Me.Btnbaglan.Size = New System.Drawing.Size(79, 41)
+        Me.Btnbaglan.TabIndex = 141
+        Me.Btnbaglan.Text = "Bağlan"
+        Me.Btnbaglan.UseVisualStyleBackColor = False
+        '
+        'CheckBox200
+        '
+        Me.CheckBox200.AutoSize = True
+        Me.CheckBox200.Enabled = False
+        Me.CheckBox200.Location = New System.Drawing.Point(434, 75)
+        Me.CheckBox200.Margin = New System.Windows.Forms.Padding(2)
+        Me.CheckBox200.Name = "CheckBox200"
+        Me.CheckBox200.Size = New System.Drawing.Size(15, 14)
+        Me.CheckBox200.TabIndex = 142
+        Me.CheckBox200.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(452, 24)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 143
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(162, Byte))
+        Me.Label2.Location = New System.Drawing.Point(286, 15)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(249, 25)
+        Me.Label2.TabIndex = 144
+        Me.Label2.Text = "LD250 ASRS Programı"
+        '
+        'PLC_okumaTimer
+        '
+        Me.PLC_okumaTimer.Interval = 4000
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 434)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(1275, 20)
+        Me.TextBox1.TabIndex = 145
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 460)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(1275, 20)
+        Me.TextBox2.TabIndex = 146
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(12, 486)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(1275, 20)
+        Me.TextBox3.TabIndex = 147
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(13, 512)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(1275, 20)
+        Me.TextBox4.TabIndex = 148
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(13, 538)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(1275, 20)
+        Me.TextBox5.TabIndex = 149
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(13, 564)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(1275, 20)
+        Me.TextBox6.TabIndex = 150
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1299, 640)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CheckBox200)
+        Me.Controls.Add(Me.Btnbaglan)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Private WithEvents timer1 As Timer
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Private WithEvents btnStop As Button
+    Private WithEvents Btnbaglan As Button
+    Friend WithEvents CheckBox200 As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents PLC_okumaTimer As Timer
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+End Class
---
+++ sqveri/Form1.vb
@@ -0,0 +1,262 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+Imports S7.Net
+Imports Microsoft.VisualBasic
+Imports Microsoft.VisualBasic.CompilerServices
+Imports Mobile.Communication.Client
+Imports System.Data.SqlClient
+Imports System.Diagnostics
+Imports System.IO
+Imports System.Net
+Imports System.Runtime.CompilerServices
+Imports System.Threading
+Imports System.Data.DataTable
+Imports System.Data.OleDb
+
+
+
+
+
+
+
+
+Public Class Form1
+    Public client1 = New RobotClient("toolkitadmin", IPAddress.Parse("192.168.15.100"), "7171")
+    Public sayacadresi(68) As String
+    Public Masa(68) As String
+    Public Konveyor(68) As String
+    Public SonSayac(68) As String
+    Public iskon As String
+    Public konsayi As Integer
+
+    Dim baglantiASRS As New SqlClient.SqlConnection("Server=192.168.90.240; Database=2Bee_GelalSock;User Id= gelal;Password=gelal123")
+    Dim baglantiASRS1 As New SqlClient.SqlConnection("Server=192.168.55.240; Database=OrguPlc_Omron;User Id= sa;Password=Sa123Sa")
+
+    Dim baglantiASRSis As New SqlClient.SqlConnection("Server=192.168.90.240; Database=2Bee_GelalSock;User Id= gelal;Password=gelal123")
+    Dim baglantiASRSis1 As New SqlClient.SqlConnection("Server=192.168.55.240; Database=OrguPlc_Omron;User Id= sa;Password=Sa123Sa")
+
+
+    Dim yazmaBaglanti As New SqlClient.SqlConnection("Server=192.168.55.240; Database=OrguPlc_Omron;User Id= sa;Password=Sa123Sa")
+    Dim yazmaBaglantisayi As New SqlClient.SqlConnection("Server=192.168.55.240; Database=OrguPlc_Omron;User Id= sa;Password=Sa123Sa")
+    Dim okumaBaglantisayi As New SqlClient.SqlConnection("Server=192.168.55.240; Database=OrguPlc_Omron;User Id= sa;Password=Sa123Sa")
+
+    Dim Guncellemebaglanti As New SqlClient.SqlConnection("Server=192.168.90.240; Database=2Bee_GelalSock;User Id= gelal;Password=gelal123")
+    Dim Guncellemebaglanti1 As New SqlClient.SqlConnection("Server=192.168.55.240; Database=OrguPlc_Omron;User Id= sa;Password=Sa123Sa")
+
+    Public plc As New Plc(CpuType.S71500, "192.168.15.60", 0, 1)
+
+
+    Dim ASRSkomut As New SqlClient.SqlCommand
+    Dim ASRSkomutis As New SqlClient.SqlCommand
+    Dim YazmaKomut As New SqlClient.SqlCommand
+    Dim YazmaKomutsayi As New SqlClient.SqlCommand
+    Dim OkumaKomutSayi As New SqlClient.SqlCommand
+    Dim GuncellemeKomut As New SqlClient.SqlCommand
+
+
+    Dim kapiisvar(7) As Boolean
+    Dim data As New DataSet
+
+    Public Sub goster()
+        baglantiASRS.Open()
+        data.Clear()
+        Dim adaptor As New SqlClient.SqlDataAdapter("select * from [dbo].[IC_KasaHazir]", baglantiASRS)
+        adaptor.Fill(data, "sanal")
+        DataGridView1.DataSource = data.Tables("sanal")
+        baglantiASRS.Close()
+    End Sub
+    Public Sub is_gonder()
+        ASRSkomut.CommandText = "Select kh_isemrino,kh_kapiid,kh_masaid, count(kh_kat) as kacsira 
+FROM [dbo].[IC_KasaHazir] 
+where kh_kat=1 and kh_hazir=1 and kh_masaid<>0 and kh_gorevvar<>'True'  
+group by kh_isemrino,kh_kapiid,kh_masaid having count(kh_kat)>0"
+        baglantiASRS.Open()
+
+
+        ASRSkomut.Connection = baglantiASRS
+        ASRSkomut.CommandType = CommandType.Text
+        Dim okunan As SqlDataReader
+        okunan = ASRSkomut.ExecuteReader()
+        While (okunan.Read())
+
+            If okunan("kh_kapiid") IsNot "" Then
+                Dim i As Integer
+
+                konsayi = 1
+                i = 1
+                While i < sayacadresi.Count
+                    If okunan("kh_masaid") = Convert.ToInt32(Masa(i).Replace("Formhane", "")) And SonSayac(i) < 7 Then
+
+                        Exit While
+
+                    Else
+                        If okunan("kh_masaid") = Convert.ToInt32(Masa(i).Replace("Formhane", "")) And SonSayac(i) > 7 Then
+                            konsayi += 1
+
+                        End If
+
+                        'TextBox3.Text = SonSayac(i)
+                        'TextBox2.Text = "Formhane" & okunan("kh_masaid")
+                    End If
+                    i += 1
+                End While
+
+
+                If 0 < konsayi And konsayi < 4 Then
+
+                    Dim ex As System.Exception
+                    Try
+                        client1.sendcommand("queueMulti 2 2 " & "ASRSKapi" & okunan("kh_kapiid") & "-" & okunan("kacsira") & " pickup 10 LD250_Formhane" & okunan("kh_masaid") & "-" & konsayi & " dropoff 20")
+                        TextBox1.Text = "queueMulti 2 2 " & "ASRSKapi" & okunan("kh_kapiid") & "-" & okunan("kacsira") & " pickup 10 LD250_Formhane" & okunan("kh_masaid") & "-" & iskon & " dropoff 20"
+
+                        baglantiASRSis.Open()
+                        ASRSkomutis.Connection = baglantiASRSis
+                        ASRSkomutis.CommandType = CommandType.Text
+                        ASRSkomutis.CommandText = "Select * from [dbo].[IC_KasaHazir] where kh_kapiid='" & okunan("kh_kapiid") & "' and kh_isemrino='" & okunan("kh_isemrino") & "'"
+                        Dim okunanis As SqlDataReader
+                        okunanis = ASRSkomutis.ExecuteReader()
+                        yazmaBaglanti.Open()
+                        YazmaKomut.Connection = yazmaBaglanti
+                        Guncellemebaglanti.Open()
+                        GuncellemeKomut.Connection = Guncellemebaglanti
+
+                        While (okunanis.Read())
+                            Dim istasyon As String
+                            If okunanis("kh_kapiid").ToString.Length = 1 Then
+                                istasyon = "0" & okunanis("kh_kapiid").ToString
+                            Else
+                                istasyon = okunanis("kh_kapiid").ToString
+                            End If
+                            GuncellemeKomut.CommandText = "UPDATE [dbo].[IC_KasaHazir] SET [kh_gorevvar] = 'True' WHERE kh_kasaid='" & okunanis("kh_kasaid") & "'"
+                            GuncellemeKomut.ExecuteNonQuery()
+
+
+
+
+                            YazmaKomut.CommandText = "INSERT INTO [dbo].[YMASRS_HazirKasaTablosu]
+           ([IsEmriNumarasi]
+           ,[KasaID]
+           ,[ForhaneIsIstasyonu]
+           ,[Tarih])
+     VALUES
+           ('" & okunanis("kh_isemrino") & "'
+           ,'" & okunanis("kh_kasaid") & "'
+           ,'Formhane" & istasyon & "'
+           ,Getdate())"
+                            YazmaKomut.ExecuteNonQuery()
+                        End While
+                        Guncellemebaglanti.Close()
+                        baglantiASRSis.Close()
+
+                        Label1.Text = "Bağlantı Sağladı"
+                        CheckBox200.Checked = True
+
+                    Catch ex
+                        Label1.Text = "Bağlantı Düştü"
+                        CheckBox200.Checked = False
+                    End Try
+                End If
+                Exit While
+                End If
+
+
+        End While
+
+        baglantiASRS.Close()
+        yazmaBaglanti.Close()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call degiskenatama()
+        Call goster()
+
+
+
+    End Sub
+
+    Private Sub timer1_Tick(sender As Object, e As EventArgs) Handles timer1.Tick
+        Call goster()
+        Call is_gonder()
+
+    End Sub
+
+    Private Sub Btnbaglan_Click(sender As Object, e As EventArgs) Handles Btnbaglan.Click
+        Try
+
+            client1.Password = "1234"
+            client1.Connect()
+
+            CheckBox200.Checked = True
+            Label1.Text = "Bağlantı Sağlandı"
+            PLC_okumaTimer.Start()
+            timer1.Start()
+
+
+        Catch exception1 As Exception
+
+            MsgBox("Robotlara bağlantı sağlanamadı. Lütfen internet ayarlarını kontrol ettikten sonra tekrar deneyiniz.")
+            Exit Sub
+
+        End Try
+
+    End Sub
+
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        Close()
+
+    End Sub
+
+    Private Sub PLC_okumaTimer_Tick(sender As Object, e As EventArgs) Handles PLC_okumaTimer.Tick
+        Dim i As Integer
+        i = 1
+        While i < sayacadresi.Count
+
+            plc.Open()
+
+
+            yazmaBaglantisayi.Open()
+            YazmaKomutsayi.Connection = yazmaBaglantisayi
+            YazmaKomutsayi.CommandText = "UPDATE [dbo].[YMASRS_FormhaneIstasyonKasaSayıları]
+   SET [KasaSayisi] = '" & plc.Read(sayacadresi(i)) & "'
+ WHERE [MakineNumarasi]='" & Masa(i) & "' and [Konveyor]='" & Konveyor(i) & "'"
+            SonSayac(i) = plc.Read(sayacadresi(i))
+            YazmaKomutsayi.ExecuteNonQuery()
+            yazmaBaglantisayi.Close()
+            plc.Close()
+
+
+            i += 1
+        End While
+
+
+    End Sub
+
+    Public Sub degiskenatama()
+        OkumaKomutSayi.CommandText = "select * from [dbo].[YMASRS_Formhane_Kon_Sayac_Adresleri]"
+        okumaBaglantisayi.Open()
+        OkumaKomutSayi.Connection = okumaBaglantisayi
+        OkumaKomutSayi.CommandType = CommandType.Text
+        Dim okunan2 As SqlDataReader
+        okunan2 = OkumaKomutSayi.ExecuteReader()
+        Dim i As Integer
+
+        i = 1
+
+        While okunan2.Read()
+
+                sayacadresi(i) = (okunan2("adres"))
+                Masa(i) = (okunan2("MakineNumarasi"))
+                Konveyor(i) = (okunan2("Konveyor"))
+                i += 1
+            End While
+            okumaBaglantisayi.Close()
+
+    End Sub
+End Class
+
